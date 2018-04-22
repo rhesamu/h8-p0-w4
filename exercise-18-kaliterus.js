f45d0b1b9fe99b24db314e6bea9f7da75a2005c5
Rhesa Utomo <venderblitz@gmail.com>
2018-04-22 18:33:29 +0700

    fixed exercise 18

--- exercise-18-kaliterus.js
+++ exercise-18-kaliterus.js
@@ -1,12 +1,22 @@
+/*
+Problem
+-------
+Diberikan sebuah function kaliTerusRekursif(angka) yang menerima satu parameter berupa angka. Function akan memproses angka yang lebih dari satu digit menjadi satu digit dengan melakukan perkalian. Bila masih lebih dari satu digit, terus lakukan perkalian setiap digit-nya untuk pada akhirnya mendapatkan satu digit. Wajib menggunakan rekursif untuk berlatih rekursif, namun kamu boleh menggunakan looping juga disini.
+
+Contoh:
+-------
+3 => 3, karena sudah satu digit 24 => 8. Karena 24 dua digit, maka kita lakukan 2 * 4 = 8. 8 satu digit, maka hasilnya adalah 8. 654 => 0. Karena 654 tiga digit, maka lakukan 6 * 5 * 4 = 120. 120 tiga digit, maka lakukan 1 * 2 * 0 = 0. 0 satu digit, maka hasilnya adalah 0.
+*/
+
 function kaliTerusRekursif(angka) {
   // you can only write your code here!
   var number = String(angka);
-  var temp = 1;
 
   if (number.length === 1) {
     return angka;
   }
   else {
+    var temp = 1;
     for(var i = 0; i < number.length; i++) {
       temp = temp * Number(number[i]);
     }
